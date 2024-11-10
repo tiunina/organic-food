f768228e412d584a0b861053a5e22a64cc1fbf61
Alona Tiunina <163348743+tiunina@users.noreply.github.com>
2024-11-10 21:42:13 +0200

    hero not finished

---
+++ src/js/hero.js
@@ -0,0 +1,16 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+setTimeout(function tick() {
+  swiper.slideNext(1500);
+  setTimeout(tick, 2000);
+}, 2000);
