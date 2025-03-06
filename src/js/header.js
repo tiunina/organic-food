6cb2c160c6f2e7ef9e102decc8cf3689318e3050
Alona Tiunina <163348743+tiunina@users.noreply.github.com>
2025-03-06 14:16:38 +0200

    check the fix

--- src/js/header.js
+++ src/js/header.js
@@ -1,25 +1,59 @@
 const burger = document.querySelector('.burger-btn');
 const closeBtn = document.querySelector('.modal-close-btn');
-const orderModalBtn = document.querySelector('#mobile-order-btn');
 const mobileMenu = document.querySelector('.modal-overley');
+
+const orderModalBtn = document.querySelector('#mobile-order-btn');
+const contactModal = document.querySelector('.contact-modal');
+const contactCloseBtn = document.querySelector('.contact-close-btn');
+
+const contactHeader = document.querySelector('.contact-btn');
 
 burger.addEventListener('click', toggleMobileMenu);
 closeBtn.addEventListener('click', toggleMobileMenu);
-orderModalBtn.addEventListener('click', toggleMobileMenu);
+
+if (orderModalBtn) {
+  orderModalBtn.addEventListener('click', () => {
+    mobileMenu.classList.remove('is-open');
+    contactModal.classList.add('is-open');
+    document.body.style.overflow = 'hidden'; // Prevent scrolling when open
+  });
+}
+
+if (contactHeader) {
+  contactHeader.addEventListener('click', () => {
+    contactModal.classList.add('is-open');
+    document.body.style.overflow = 'hidden';
+  });
+}
+
+if (contactCloseBtn) {
+  contactCloseBtn.addEventListener('click', () => {
+    contactModal.classList.remove('is-open');
+    document.body.style.overflow = '';
+    // document.body.style.overflow = 'hidden';
+    // mobileMenu.classList.add('is-open');
+  });
+}
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape' && contactModal.classList.contains('is-open')) {
+    contactModal.classList.remove('is-open');
+    document.body.style.overflow = '';
+  }
+});
 
 function toggleMobileMenu() {
   const isOpen = mobileMenu.classList.toggle('is-open');
 
   if (isOpen) {
     document.body.style.overflow = 'hidden';
-    document.querySelector('.section_hero').style.display = 'none'; // Hide hero section
+    // document.querySelector('.section_hero').style.display = 'none';
   } else {
     document.body.style.overflow = '';
-    document.querySelector('.section_hero').style.display = 'block'; // Show hero section again
+    // document.querySelector('.section_hero').style.display = 'block';
   }
 }
 
-// Close mobile menu on "Escape" key press
 document.addEventListener('keydown', event => {
   if (event.key === 'Escape' && mobileMenu.classList.contains('is-open')) {
     toggleMobileMenu();
